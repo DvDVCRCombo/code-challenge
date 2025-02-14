edacfc11aada4d92729c9a62c0522e0709226569
Crunchy <126545480+DvDVCRCombo@users.noreply.github.com>
2025-02-14 15:18:13 -0600

    Throw exception if there are negative numbers
    - Throw an exception if there are negative numbers
    - Include the negative numbers in the exception

--- Services/CalculatorService.cs
+++ Services/CalculatorService.cs
@@ -14,18 +14,21 @@ namespace CodeChallenge.Services
         {
             var stringParts = GetStringParts(input);
 
-            var numbers = stringParts.Select(x => {
+            var numbers = stringParts.Select(x =>
+            {
                 var result = 0;
                 int.TryParse(x, out result);
                 return result;
             });
+
+            ValidateNoNegativeNumbers(numbers);
 
             return numbers.Sum();
         }
 
         internal IList<string> GetStringParts(string input)
         {
-            if(input == null || string.IsNullOrEmpty(input))
+            if (input == null || string.IsNullOrEmpty(input))
             {
                 return new List<string>();
             }
@@ -33,6 +36,16 @@ namespace CodeChallenge.Services
             var stringParts = input.Split(_delimiters, StringSplitOptions.None);
 
             return stringParts;
+        }
+
+        internal void ValidateNoNegativeNumbers(IEnumerable<int> values)
+        {
+            var negativeNumbers = values.Where(x => x < 0).Select(x => x).ToList();
+
+            if (negativeNumbers.Any())
+            {
+                throw new Exception($"Input string contained the following negative numbers: {string.Join(",", negativeNumbers)}");
+            }
         }
     }
 }
